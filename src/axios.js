0c08e852ec20530762ac32f65c755389e8567d15
Fábio Gonçalves <fabiobasquete4@hotmail.com>
2019-02-27 00:11:03 -0300

    Curso vue JS

--- src/axios.js
+++ src/axios.js
@@ -1,16 +1,51 @@
 import axios from 'axios'
 import config from './config/config'
 
+
+
 axios.defaults.baseURL = config.apiURL
 
 //export default axios
 
-/*export default axios.create({
-    baseURL: config.apiURL
-})*/
+//export default axios.create({
+//  baseURL: config.apiURL,    
+//})
+
 
 const instance = axios.create({})
 
-instance.defaults.baseURL= config.apiURL
+//instance.defaults.baseURL= config.apiURL
+
+instance.interceptors.request.use(config => {
+    console.log('Interceptando requisição: ', config)
+    config.data = {
+        ...config.data,
+        curso: 'Vue JS'
+    }
+    config.headers.common['Authorization'] = 'Bearer toke_jwt'
+    config.headers.put['Meu-Cabecalho'] = 'Curso VUEJS'
+    return config
+    /*return new Promise((resolve, reject) => {
+        console.log('Fazendo requisição aguardar');
+        setTimeout(() => {
+            console.log('Enviando requisição');
+            resolve(config)
+        }, 2000);
+    })*/
+}, error => {
+    console.log('Erro ao fazer requisição: ', erro);
+    return new Promise.reject(error)
+})
+
+instance.interceptors.response.use(response => {
+    console.log('Interceptando resposta... ', response)
+    if (Array.isArray(response.data)) {
+        response.data = response.data.slice(1, 3)
+    }
+    return response
+}, error => {
+    console.log('Erro capturado no interceptador de resposta: ', error);
+ return Promise.reject(error)
+})
 
 export default instance
